20171fadbdfbed7a757295870bcab029f484efe6
KComrade53 <kcomrade53@gmail.com>
2019-01-27 23:01:57 -0500

    More increment functionality added

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 class Counter extends React.Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
+    }
+
+    handleIncrement = () => {
+        console.log('Increment clicked', this);
     };
 
     render() {
@@ -11,7 +22,17 @@ class Counter extends React.Component {
                 <span className={this.getBadgeClasses()}>
                     {this.formatCount()}
                 </span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button
+                    onClick={this.handleIncrement}
+                    className="btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>
+                <ul>
+                    {this.state.tags.map(tag => (
+                        <li key={tag}>{tag}</li>
+                    ))}
+                </ul>
             </div>
         );
     }

7e2ffa8d40acb7b249adbf3f2ced0101e4d600d6
KComrade53 <kcomrade53@gmail.com>
2019-01-27 23:16:37 -0500

    Functional Increment

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -13,7 +13,8 @@ class Counter extends React.Component {
     }
 
     handleIncrement = () => {
-        console.log('Increment clicked', this);
+        // console.log('Increment clicked', this);
+        this.setState({ count: this.state.count + 1 });
     };
 
     render() {
